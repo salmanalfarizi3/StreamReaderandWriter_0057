fc144fe8e0300c59d09c0a3b3cb547e187d975a5
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-07-09 21:34:30 +0700

    Tambah loop untuk menulis isi file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -17,4 +17,14 @@ int main()
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+            break;
+        // menulis dan memasukan nilai dari 'baris' kedlam file
+        outfile << baris << endl;
     
