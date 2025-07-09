0620fa5ae6decdb72d3810d152a29bac2377034e
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-07-09 21:45:29 +0700

    Tambahkan output setelah throw (yang tidak akan dieksekusi)

--- manual.cpp
+++ manual.cpp
@@ -6,4 +6,8 @@ int main()
     try {
         cout << "Selamat belajar di prodi TI UMY" <<endl;
         throw 0,5; //melemparkan sebuah integer maka
-       
+        cout << "Pernyataan tidak akan dieksekusi" <<endl;
+    }
+
+    
+}
