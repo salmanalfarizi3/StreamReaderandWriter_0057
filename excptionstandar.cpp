16014fea5bd8cd37ea8c24e5ca82ac827893d0d9
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-07-09 21:24:36 +0700

    Menambahkan blok try-catch

--- excptionstandar.cpp
+++ excptionstandar.cpp
@@ -5,3 +5,13 @@
 #include <array>
 
 //untuk array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    cout << "awal program" <<endl; //penanda 1:......
+    try {
+        array <int, 3> data= {10,20,30};
+        //pesan array integer 3 elemen
+        cout << data.at (5) <<endl;
+    }
